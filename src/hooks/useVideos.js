b9dcbcc9e107c778f0667dd34e7cf8d4c5c84706
Pablo Rodriguez <pablorodriguez88@gmail.com>
2021-03-14 03:41:00 -0300

    Solucionado Warning

--- src/hooks/useVideos.js
+++ src/hooks/useVideos.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import youtube from "../apis/youtube";
 
 const useVideos = (defaultSearchTerm) => {
